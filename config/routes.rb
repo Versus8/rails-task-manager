3794e022823eeecefa0b0716248fcecd33f8ef4d
Veronique Tureau <veronique.tureau@gmail.com>
2023-10-14 14:28:34 -0400

    refactoring forms

--- config/routes.rb
+++ config/routes.rb
@@ -3,16 +3,18 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
-  get 'tasks', to: 'tasks#index'
 
-  get 'tasks/new', to: 'tasks#new'
-  post 'tasks', to: 'tasks#create'
+  # get 'tasks', to: 'tasks#index'
 
-  get 'tasks/:id', to: 'tasks#show', as: :task
+  # get 'tasks/new', to: 'tasks#new'
+  # post 'tasks', to: 'tasks#create'
 
-  get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
-  patch 'tasks/:id', to: 'tasks#update'
+  # get 'tasks/:id', to: 'tasks#show', as: :task
 
-  delete 'tasks/:id', to: 'tasks#destroy'
+  # get 'tasks/:id/edit', to: 'tasks#edit', as: :edit
+  # patch 'tasks/:id', to: 'tasks#update'
 
+  # delete 'tasks/:id', to: 'tasks#destroy'
+
+  resources :tasks
 end
